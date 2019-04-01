7cf79beb6b5aa99a483b1ade7f4704e4f4cfb56d
Florian Matter <florianmatter@gmail.com>
2019-04-01 17:50:36 +0200

    migrated exemplified morphemes for maquiritari

--- cariban_morphemes/__init__.py
+++ cariban_morphemes/__init__.py
@@ -8,8 +8,15 @@ from cariban_morphemes import models
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    settings['route_patterns'] = {
+        'unitparameters': '/functions',
+        'unitparameter': '/function/{id:[^/\.]+}',
+        'units': '/morphemes',
+        'unit': '/morpheme/{id:[^/\.]+}',
+        'parameters': '/cognatesets',
+        'parameter': '/cognateset/{id:[^/\.]+}',
+
+    }
     config = Configurator(settings=settings)
     config.include('clld.web.app')
-    # config.register_resource(
-    #     'morpheme', Morpheme, IMorpheme, with_index=True)
     return config.make_wsgi_app()
